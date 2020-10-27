7b7f31aa9e0c253fe08630b5b59bf8a5c40b808b
gnarlee-gee <charlesgrealy@gmail.com>
2020-10-27 13:30:24 -0500

    apply more styles to buttons

---
+++ script/calculator.js
@@ -0,0 +1,18 @@
+const mathExp = [];
+
+
+
+// document.querySelector('.calc-display').innerHTML = 'this';
+// document.querySelector('.calc-display').innerHTML = 'that';
+
+
+// let input = '';
+// console.log(input);
+// input += '3';
+// input += 4;
+// console.log(input);
+// test();
+// console.log(input);
+// function test(){
+//     input = '';
+// }
